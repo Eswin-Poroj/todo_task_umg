a67718f561616aecfbd04aec30fac2e98a778e5d
Eswin Poroj <eporojc1@miumg.edu.gt>
2025-05-01 20:17:21 -0600

    Create the colors file

---
+++ lib/utilis/theme/colors_app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  // Background
+  static Color get background => Color(0xFFF3F2F1);
+  static Color get backgroundTextField => Color(0xFFFFFFFF);
+
+  // Button
+  static Color get buttonBackground => Color(0xFF0078D4);
+  static Color get textButtonColor => Color(0xFF323130);
+
+  // Text Color
+  static Color get primaryTextColor => Color(0xFF000000);
+  static Color get secondaryTextColor => Color(0xFF757575);
+  static Color get thirdTextColor => Color(0xFF1E1E1E);
+  static Color get textColorError => Color(0xFF903409);
+  static Color get textColorLabel => Color(0xFF1E1E1E);
+
+  // Drawer
+  static Color get backgroundColorButtonDrawer => Color(0xFFE8DEF8);
+  static Color get textColorButtonDrawer => Color(0xFF49454F);
+
+  // Check List
+  static Color get colorBackgroundCheckList => Color(0xFF65558F);
+}
---
+++ lib/utilis/theme/theme_data_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ThemeDataApp {
+  static ThemeData themeData() {
+    return ThemeData(
+      // Input decoration
+      inputDecorationTheme: InputDecorationTheme(
+        labelStyle: TextStyle(
+          color: Color.
+        )
+      )
+    );
+  }
+}
