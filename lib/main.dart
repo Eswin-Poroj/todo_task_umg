aab213f9c4eb8aa4b25370f21d01917189ca67ce
Eswin Poroj <eporojc1@miumg.edu.gt>
2025-05-05 00:17:44 -0600

    add providers in void main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_task_umg/services/services_user.dart';
 import 'package:todo_task_umg/utilis/routes/routes.dart';
 import 'package:todo_task_umg/utilis/theme/theme_data_app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => ServicesUser())],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
