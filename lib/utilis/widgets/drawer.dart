63cc32da671d8c4e21a7fd3b37122c3311735a10
Eswin Poroj <eporojc1@miumg.edu.gt>
2025-05-08 23:57:36 -0600

    add drawer widget file

--- lib/utilis/widgets/drawer.dart
+++ lib/utilis/widgets/drawer.dart
@@ -1,22 +1,69 @@
 import 'package:flutter/material.dart';
 
-class WidgetDrawer extends StatelessWidget {
-  const WidgetDrawer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            decoration: BoxDecoration(color: Colors.blue),
-            child: Text('Header'),
-          ),
-          ListTile(title: Text('Item 1'), onTap: () {}),
-          ListTile(title: Text('Item 2'), onTap: () {}),
-        ],
-      ),
-    );
-  }
+Drawer drawerApp(BuildContext context) {
+  return Drawer(
+    width: 30,
+    child: ListView(
+      padding: EdgeInsets.all(16.0),
+      children: <Widget>[
+        const DrawerHeader(child: Text('TODO TASK')),
+        ListTile(
+          contentPadding: EdgeInsets.all(8.0),
+          leading: const Icon(Icons.calendar_month),
+          title: Text('Hoy'),
+          onTap: () {},
+        ),
+        ListTile(
+          contentPadding: EdgeInsets.all(8.0),
+          leading: const Icon(Icons.favorite_border_outlined),
+          title: Text('Favoritas'),
+          onTap: () {},
+        ),
+        ListTile(
+          contentPadding: EdgeInsets.all(8.0),
+          leading: const Icon(Icons.check),
+          title: Text('Completas'),
+          onTap: () {},
+        ),
+        ListTile(title: Text('Estado')),
+        ListTile(
+          contentPadding: EdgeInsets.all(8.0),
+          leading: const Icon(Icons.priority_high),
+          title: Text('Pendientes'),
+          onTap: () {},
+        ),
+        ListTile(
+          contentPadding: EdgeInsets.all(8.0),
+          leading: const Icon(Icons.priority_high),
+          title: Text('En Curso'),
+          onTap: () {},
+        ),
+        ListTile(
+          contentPadding: EdgeInsets.all(8.0),
+          leading: const Icon(Icons.priority_high),
+          title: Text('Completadas'),
+          onTap: () {},
+        ),
+        ListTile(title: Text('Prioridad')),
+        ListTile(
+          contentPadding: EdgeInsets.all(8.0),
+          leading: const Icon(Icons.priority_high),
+          title: Text('Prioridad Alta'),
+          onTap: () {},
+        ),
+        ListTile(
+          contentPadding: EdgeInsets.all(8.0),
+          leading: const Icon(Icons.low_priority),
+          title: Text('Prioridad Media'),
+          onTap: () {},
+        ),
+        ListTile(
+          contentPadding: EdgeInsets.all(8.0),
+          leading: const Icon(Icons.low_priority),
+          title: Text('Prioridad Baja'),
+          onTap: () {},
+        ),
+      ],
+    ),
+  );
 }
